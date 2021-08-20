5be40507d8d90b38cf898137cadee27e2f22249a
Zebedee <zebedeeslewis@gmail.com>
2021-08-20 15:30:18 -0600

    refactor: expose domain entity accessors

--- libs/domain/entity/test_case/__exports__.py
+++ libs/domain/entity/test_case/__exports__.py
@@ -4,4 +4,10 @@ from .test_case import (
   RootModel,
   get_steps,
   set_steps,
+  set_id,
+  get_id,
+  set_description,
+  get_description,
+  set_execution_state,
+  get_execution_state,
   run,)
