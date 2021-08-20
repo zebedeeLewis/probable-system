9a3bf570b21b488c4b4c352610c10e2192e36d9b
Zebedee <zebedeeslewis@gmail.com>
2021-08-20 15:44:07 -0600

    refactor: implement test case DAO functionality

--- libs/infrastructure/data_access/test_case/test_case_dao.py
+++ libs/infrastructure/data_access/test_case/test_case_dao.py
@@ -11,24 +11,36 @@ import openpyxl.workbook.workbook as workbook
 
 # Local Libs
 from libs.infrastructure.data_access.dao_interface import DataAccessObjectI
+from libs.infrastructure.data_transfer.test_case import TestCaseDTO
 from libs.domain.entity.test_case import TestCase
 
 
-WORKSHEET_NAME: Final[str] = "Sheet1"
+WORKSHEET_NAME: Final[str] = "test_case_table"
 
 
 class TestCaseDAO(DataAccessObjectI.DataAccessObjectI):
 
   def __init__(self, dataWorkbook: workbook.Workbook):
-    self.dataWorkbook = dataWorkbook
+    self.worksheet = dataWorkbook[WORKSHEET_NAME]
 
 
-  # TODO!!!
-  def read(
-      self,
-      id: Optional[str]=None
-      ) -> Union[None, TestCase.Model, List[TestCase.Model]]:
-    return [] if id == None else None
+  def read_set(self) -> List[TestCase.Model]:
+    return [ TestCaseDTO.parse_row_to_model(row_index, self.worksheet)
+      for row_index in range(self.worksheet.min_row, self.worksheet.max_row+1)]
+
+
+  def read_single(self, id: str) -> Union[None, TestCase.Model]:
+    test_cases = self.read_set()
+    for test_case in test_cases:
+      if TestCase.get_id(test_case) == id:
+        return test_case
+
+    return None
+
+
+  def read(self, id: Optional[str]=None) -> Union[None, TestCase.Model, List[TestCase.Model]]:
+    return (self.read_set() if id == None
+      else self.read_single(id))
 
 
 def create(dataWorkbook: workbook.Workbook) -> TestCaseDAO:
